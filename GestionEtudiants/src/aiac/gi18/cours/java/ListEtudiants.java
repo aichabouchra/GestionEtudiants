6427ea56044f3497594991ba75965bf52627040b
hp <hp@10.30.255.9>
2022-04-15 10:42:36 +0000

    2. b. la redéfinition de la méthode add

--- GestionEtudiants/src/aiac/gi18/cours/java/ListEtudiants.java
+++ GestionEtudiants/src/aiac/gi18/cours/java/ListEtudiants.java
@@ -3,5 +3,8 @@ package aiac.gi18.cours.java;
 import java.util.ArrayList;
 
 public class ListEtudiants extends ArrayList<Etudiant>{
-
+	public boolean add(Etudiant e) {
+		if (this.contains(e.id)) return false;
+		return super.add(e);
+	}
 }
