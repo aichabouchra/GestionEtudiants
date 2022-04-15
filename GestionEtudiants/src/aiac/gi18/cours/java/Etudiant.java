4d25da632e93d091ed053b6cac321e4fdc143e04
hp <hp@10.30.255.9>
2022-04-15 10:31:44 +0000

    e. l'implémentation de l'interface Comparable

--- GestionEtudiants/src/aiac/gi18/cours/java/Etudiant.java
+++ GestionEtudiants/src/aiac/gi18/cours/java/Etudiant.java
@@ -1,5 +1,7 @@
 package aiac.gi18.cours.java;
 
+import aiac.gi18.personne.Etudiant;
+
 public class Etudiant implements Comparable<Etudiant>{
 	private String nom;
 	private int  id;
@@ -20,5 +22,11 @@ public class Etudiant implements Comparable<Etudiant>{
 	public boolean equals(Object obj) {
 		if (obj.id == this.id) return true;
 		return false;
+	}
+	
+	public int compareTo(Etudiant e) {
+		if (this.note > e.note ) return -1;
+		if (this.note < e.note) return 1;
+		return 0;
 	}
 }
