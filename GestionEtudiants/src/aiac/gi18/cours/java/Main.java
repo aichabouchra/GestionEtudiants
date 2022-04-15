6bc29deb9a82e81dff4fd2b13db649573b5cb1a8
hp <hp@10.30.255.9>
2022-04-15 11:53:11 +0000

    3. d. l'affichage

--- GestionEtudiants/src/aiac/gi18/cours/java/Main.java
+++ GestionEtudiants/src/aiac/gi18/cours/java/Main.java
@@ -9,13 +9,12 @@ public class Main {
 		Etudiant c3=new Etudiant(1,"youssef");
 		
 		c1.setNote(15); c2.setNote(12); c3.setNote(16);
-
-		c1.setNote(15); c2.setNote(12); c3.setNote(16);
-
+		
 		ListEtudiants mesEtudiants=new ListEtudiants();
 		mesEtudiants.add(c1);
 		mesEtudiants.add(c2);
 		mesEtudiants.add(c3);
 		
+		System.out.println(mesEtudiants);
 	}
 }
