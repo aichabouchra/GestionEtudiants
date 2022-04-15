3d4f3d96ac48fd2ec781b5eb26bb29a1da8ca330
hp <hp@10.30.255.9>
2022-04-15 22:44:05 +0000

    2. c. l'implémentation de la méthode estVrai

--- GestionEtudiants/src/aiac/gi18/cours/java/ListEtudiants.java
+++ GestionEtudiants/src/aiac/gi18/cours/java/ListEtudiants.java
@@ -2,6 +2,8 @@ package aiac.gi18.cours.java;
 
 import java.util.ArrayList;
 
+import aiac.gi18.java.complexe.Etudiant;
+
 public class ListEtudiants extends ArrayList<Etudiant> implements aiac.gi18.cours.java.Condition{
 	public boolean add(Etudiant e) {
 		if (this.contains(e)) {
@@ -11,4 +13,9 @@ public class ListEtudiants extends ArrayList<Etudiant> implements aiac.gi18.cour
 		return super.add(e); 
 	}
 
+	@Override
+	public boolean estVrai(Etudiant e) {
+		if (e.getNote()>13) return true;
+		return false;
+	}
 }
