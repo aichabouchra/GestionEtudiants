511ac1df4857ee6ec346ea1c3f7f37785eb9ab71
hp <hp@10.30.255.9>
2022-04-15 11:51:36 +0000

    3. b. la modification des notes

--- GestionEtudiants/src/aiac/gi18/cours/java/Main.java
+++ GestionEtudiants/src/aiac/gi18/cours/java/Main.java
@@ -7,6 +7,8 @@ public class Main {
 		Etudiant c1=new Etudiant(1,"hamid");
 		Etudiant c2=new Etudiant(2,"driss");
 		Etudiant c3=new Etudiant(1,"youssef");
+		
+		c1.setNote(15); c2.setNote(12); c3.setNote(16);
 
 	}
 }
