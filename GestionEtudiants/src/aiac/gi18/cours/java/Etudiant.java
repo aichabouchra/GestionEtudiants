73da5e412a99ab70242eded1795290492e6314ea
hp <hp@10.30.255.9>
2022-04-15 10:24:33 +0000

    a. La déclaration

--- GestionEtudiants/src/aiac/gi18/cours/java/Etudiant.java
+++ GestionEtudiants/src/aiac/gi18/cours/java/Etudiant.java
@@ -1,5 +1,9 @@
 package aiac.gi18.cours.java;
 
-public class Etudiant {
-
+public class Etudiant implements Comparable<Etudiant>{
+	private String nom;
+	private int  id;
+	private double note;
+	
+	
 }
