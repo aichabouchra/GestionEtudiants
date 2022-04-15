4d08ff9eef75d2a993eb49612903a4c248fddb9e
hp <hp@10.30.255.9>
2022-04-15 22:45:47 +0000

    2. c. l'implémentation de la méthode filtrer

--- GestionEtudiants/src/aiac/gi18/cours/java/ListEtudiants.java
+++ GestionEtudiants/src/aiac/gi18/cours/java/ListEtudiants.java
@@ -2,7 +2,9 @@ package aiac.gi18.cours.java;
 
 import java.util.ArrayList;
 
+import aiac.gi18.java.complexe.Condition;
 import aiac.gi18.java.complexe.Etudiant;
+import aiac.gi18.java.complexe.ListEtudiants;
 
 public class ListEtudiants extends ArrayList<Etudiant> implements aiac.gi18.cours.java.Condition{
 	public boolean add(Etudiant e) {
@@ -17,5 +19,15 @@ public class ListEtudiants extends ArrayList<Etudiant> implements aiac.gi18.cour
 	public boolean estVrai(Etudiant e) {
 		if (e.getNote()>13) return true;
 		return false;
+	}
+	
+	public ListEtudiants filtrer(Condition c) {
+		int i=0; 
+		if (c.estVrai(this.get(i))==true) {
+			Etudiant a=this.get(i);
+			this.add(a);
+			i++; 
+		}
+		return this;
 	}
 }
